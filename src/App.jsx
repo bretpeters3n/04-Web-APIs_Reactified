9b53a21b41aae882e0cb5ee7346d0bdaf9faae7e
Bret Petersen <bretpetersen82@gmail.com>
2023-10-19 18:07:08 -0500

    Added photots

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ function App() {
   function startGame() {
     // setCount(60);
     timer(60, callback);
+    let reveal = document.getElementById('objectDetails');
+    reveal.style.display = 'block';
   }
   
   function timer(seconds, cb) {
@@ -27,6 +29,30 @@ function App() {
     console.log('callback');
   };
 
+  let quizQuestions = [
+    {
+      question: 'Name this knot',
+      answers:  {
+        answer01: {
+          answer: 'Answer #1',
+          correct: false,
+        },
+        answer02: {
+          answer: 'Answer #2',
+          correct: false,
+        },
+        answer03: {
+          answer: 'Answer #3',
+          correct: false,
+        },
+        answer04: {
+          answer: 'Answer #4',
+          correct: true,
+        },
+      },
+    },
+  ];
+
   return (
     <>
       <div className="utility-nav">
@@ -44,11 +70,34 @@ function App() {
         </a>
       </div>
       <h1>Knot QUIZ Challenge</h1>
-      <p>This is a timed quiz where you match illustrations of knots to their names.<br/>See your overall ranking at the end.</p>
+      <p >This is a timed quiz where you match illustrations of knots to their names.<br/>See your overall ranking at the end.</p>
       <div className="card">
         <button onClick={startGame}>
           Start Quiz presses {count}
         </button>
+      </div>
+      {/* <div id="objectDetails">{quizQuestions[0]}</div> */}
+      {/* <div id="objectDetails">{quizQuestions.map(quizQuestions => (
+        <div key={quizQuestions.question}>
+          <p>{quizQuestions.question}</p> */}
+          {/* <p>{quizQuestions.answers}</p> */}
+        {/* </div>))}
+      </div> */}
+      <div id="objectDetails">
+        {quizQuestions.map(
+          (
+            { question, answers: { answer01: { answerCopy, correct } } },
+            index
+          ) => {
+            return (
+              <div className="employees" key={index}>
+                <p>{question}</p>
+                <p>{quizQuestions.answerCopy}</p>
+                <p>{correct}</p>
+              </div>
+            )
+          }
+        )}
       </div>
     </>
   )
